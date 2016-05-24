c5a63f010585490e25d21c264397f4fc687dc311
AndyFou <a.m.founta@gmail.com>
2016-05-24 19:06:25 +0300

    Remove personal notes from script

--- yelp.py
+++ yelp.py
@@ -15,13 +15,13 @@ def main():
 
 # LOAD PHOTOS FROM FOLDER & SAVE IN A LIST [FILENAME,PHOTO,GRAYSCALE_PHOTO]
 def loadimages():
-	photos = [] 						# create "photos" list
+	photos = [] 					# create "photos" list
 	photosdir = "photos"				# set folder name ("photosdir")
 
 	for filename in os.listdir(photosdir):
 		image = cv2.imread(os.path.join(photosdir,filename))	#read photo
-		gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)			#convert to grayscale
-		photos.append([filename, image, gray])					#append in a list of photos
+		gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)		#convert to grayscale
+		photos.append([filename, image, gray])			#append in a list of photos
 
 	return photos
 
@@ -43,25 +43,3 @@ def showimage(image):
 	cv2.waitKey(0)
 
 main()
-
-
-
-# *************************************************** NOTES ********************************************************
-
-# SYSTEM
-# * load virtual environment: source cv/bin/activate
-# * mv ~/Desktop/Yelp/various/train_photos/IMAGEFILE ~/Desktop/Yelp/photos/
-
-# PYTHON
-# * Find length: len(DATATYPE)
-
-# * List: Homogenous(contains same types)	!!THE MOST CONVENIENT AND FLEXIBLE !!
-# 	Tuple: Immutable(if created, nothing can be added) & Heterogenous(can contain different types)
-#	Set: --einai sunola opote an exeis ena stoixeio sto sunolo den mporeis na baleis 2o idio --, Example Application: it can be used to find a distinct set of words
-#	Dictionary: Works like a hashmap
-
-# * Find item in nested list: photos[i][j]
-# * Print concatenation (string & int): print "Hello ",x," ",y," World"
-
-
-#Show Keypoints: showimage(cv2.drawKeypoints(photo[2],kps,None,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS))
